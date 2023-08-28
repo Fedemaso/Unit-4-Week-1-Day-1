7f7627b3b9736dc6d625d0a3bfe9ad9d8b9ffd3d
fedemaso <fede.maso1989@gmail.com>
2023-08-28 17:58:52 +0200

    Aggiungere i file di progetto.

---
+++ Day 1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1
+{
+    internal class Class1
+    {
+
+        static void Main(string[] args)
+
+        {
+            Atleta atleta = new Atleta();
+            atleta.Nome = "Cristiano";
+            atleta.Cognome = "Ronaldo";
+            atleta.Sport = "Calcio";
+            atleta.Nazionalità = "Portoghese";
+
+            atleta.GetFullAtletaProc();
+           
+
+
+            Studente studente = new Studente();
+            studente.Nome = "Robert";
+            studente.Cognome = "Oppenheimer";
+            studente.Materia = "Fisica";
+            studente.Università = "Berkeley";
+
+
+            Console.WriteLine(studente.GetFullStudente());
+            Console.ReadLine();
+
+
+
+        }
+
+       
+}
+
+
+}
---
+++ Day 1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1
+
+{
+    public class Atleta
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public string Sport { get; set; }
+
+        public string Nazionalità { get; set; }
+
+
+
+        public Atleta() { }
+
+        public Atleta(string nome, string cognome, string sport, string nazionalità)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Sport = sport;
+            Nazionalità = nazionalità;
+
+
+        }
+
+
+        public string GetFullAtleta()
+        {
+            return Nome + " " + Cognome + " " + Sport + " " + Nazionalità;
+        }
+
+        public void GetFullAtletaProc()
+        {
+            Console.WriteLine(GetFullAtleta());
+        }
+    }
+
+    public class Studente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Materia { get; set; }
+        public string Università { get; set; }
+
+
+        public Studente() { }
+
+        public Studente (string nome, string cognome, string materia, string università)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Materia = materia;
+            Università = università;
+        }
+        public string GetFullStudente()
+        {
+            return Nome + " " + Cognome + " " + Materia + " " + Università;
+        }
+
+    }
+
+} 
